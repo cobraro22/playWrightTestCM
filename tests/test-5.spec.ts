eb72ada1d14abaa2ff8e98f161b98bc618999153
Ciprian Milea <ciprian.milea@htss.ro>
2025-02-01 23:41:46 +0200

    7rd PR

--- tests/test-5.spec.ts
+++ tests/test-5.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("test", { tag: "@testulMeu5" }, async ({ page }) => {
+test("test", { tag: "@testulMeu6" }, async ({ page }) => {
   await page.goto("https://cnpgenerator.ro/");
   await page.getByLabel("Sex: *").selectOption("0");
   await page.getByRole("link", { name: "Validare CNP", exact: true }).click();
