ceb5eab26dab190c0780acc0dba13699c2b61e25
cobraro22 <cip.milea@gmail.com>
2025-02-09 14:47:33 +0200

    09.02

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,9 +1,31 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("API test", () => {
+  let tokenLogin = "";
+  const urlAPI = "https://api.practicesoftwaretesting.com";
+
+  test.beforeEach(async ({ request }) => {
+    // const urlAPI = "/";
+    const response = await request.post(urlAPI + "/users/login", {
+      data: {
+        email: "customer@practicesoftwaretesting.com",
+        password: "welcome01",
+      },
+    });
+
+    expect(response.status()).toBe(200);
+
+    const body = await response.json();
+    expect(body).toBeTruthy();
+
+    console.log(body.access_token);
+    tokenLogin = body.access_token;
+    expect(body.access_token).toBeTruthy();
+  });
+
   test("GET API", async ({ request }) => {
-    const APIurl = "https://api.practicesoftwaretesting.com";
-    const response = await request.get(APIurl + "/products");
+    const urlAPI = "https://api.practicesoftwaretesting.com";
+    const response = await request.get(urlAPI + "/products");
     expect(response.status()).toBe(200);
     expect(response.statusText()).toBe("OK");
     const body = await response.json();
@@ -13,8 +35,8 @@ test.describe("API test", () => {
   });
 
   test("Login Post /users/login ", async ({ request }) => {
-    const APIurl = "https://api.practicesoftwaretesting.com";
-    const response = await request.post(APIurl + "/users/login", {
+    const urlAPI = "https://api.practicesoftwaretesting.com";
+    const response = await request.post(urlAPI + "/users/login", {
       data: {
         email: "customer@practicesoftwaretesting.com",
         password: "welcome01",
@@ -30,7 +52,7 @@ test.describe("API test", () => {
     const token = body.access_token;
     expect(body.access_token).toBeTruthy();
 
-    // const response2 = await request.get(APIurl + "/invoices", {
+    // const response2 = await request.get(urlAPI + "/invoices", {
     //   headers: {
     //     Authorization: `Bearer ${token}`, // Setează token-ul Bearer în header
     //   },
@@ -39,7 +61,7 @@ test.describe("API test", () => {
     // // Verificăm statusul răspunsului
     expect(response.status()).toBe(200);
 
-    const response2 = await request.get(APIurl + "/invoices", {
+    const response2 = await request.get(urlAPI + "/invoices", {
       headers: {
         Authorization: `Bearer ${token}`, // Setează token-ul Bearer în header
       },
@@ -75,9 +97,9 @@ test.describe("API test", () => {
   });
 
   test("searchFilter", async ({ request }) => {
-    const APIUrl = "https://api.practicesoftwaretesting.com";
+    const urlAPI = "https://api.practicesoftwaretesting.com";
     const search = "ham";
-    const response = await request.get(APIUrl + "/products/search?q=" + search);
+    const response = await request.get(urlAPI + "/products/search?q=" + search);
     expect(response.status()).toBe(200);
 
     const body = await response.json();
@@ -106,7 +128,7 @@ test.describe("API test", () => {
 
     console.log(pictureTitles);
 
-    const response2 = await request.get(APIUrl + "/products");
+    const response2 = await request.get(urlAPI + "/products");
     expect(response2.status()).toBe(200);
 
     const body2 = await response2.json();
@@ -114,9 +136,9 @@ test.describe("API test", () => {
     // // console.log(productId);
     // // console.log(productId[0]);
 
-    // const response3 = await request.get(APIUrl + "/products/" + productId[0]);
+    // const response3 = await request.get(urlAPI + "/products/" + productId[0]);
 
-    // console.log(APIUrl + "/products/" + productId[0]);
+    // console.log(urlAPI + "/products/" + productId[0]);
 
     // expect(response3.status()).toBe(200);
     // const body3 = await response.json();
@@ -127,7 +149,7 @@ test.describe("API test", () => {
     // Verificăm dacă există produse pentru a evita erorile
     if (productIds.length > 0) {
       const firstProductId = productIds[0];
-      const url = `${APIUrl}/products/${firstProductId}`;
+      const url = `${urlAPI}/products/${firstProductId}`;
 
       console.log(url); // Logăm URL-ul pentru cerere
 
@@ -151,7 +173,7 @@ test.describe("API test", () => {
 
     // Folosește primul ID din listă
     const productId = productIds2[0];
-    const productUrl = `${APIUrl}/products/${productId}`;
+    const productUrl = `${urlAPI}/products/${productId}`;
 
     console.log("Se face request la:", productUrl);
 
@@ -169,44 +191,24 @@ test.describe("API test", () => {
     expect(body3.product_image.source_name).toContain("Unsplash");
   });
 
-  test.only("checkout", async ({ request }) => {
-    const APIurl = "https://api.practicesoftwaretesting.com";
-    const responseLogin = await request.post(APIurl + "/users/login", {
-      data: {
-        email: "customer@practicesoftwaretesting.com",
-        password: "welcome01",
-      },
-    });
-
-    expect(responseLogin.status()).toBe(200);
-
-    const bodyresponseLogin = await responseLogin.json();
-    expect(bodyresponseLogin).toBeTruthy();
-
-    console.log(bodyresponseLogin.access_token);
-    const tokenLogin = bodyresponseLogin.access_token;
-    expect(bodyresponseLogin.access_token).toBeTruthy();
-
-    const APIUrl = "https://api.practicesoftwaretesting.com";
+  test("checkout", async ({ request }) => {
+    // const urlAPI = "https://api.practicesoftwaretesting.com";
     const search = "ham";
-    const response = await request.get(APIUrl + "/products/search?q=" + search);
+    const response = await request.get(urlAPI + "/products/search?q=" + search);
     expect(response.status()).toBe(200);
 
     const body = await response.json();
     const token = body.access_token;
     console.log(body.data[0].id);
     expect(body.data.length).toBeGreaterThan(0);
-
-    const cartAPI = await request.post(`${APIUrl}/carts`, {
+    const cartAPI = await request.post(`${urlAPI}/carts`, {
       headers: {
         Authorization: `Bearer ${tokenLogin}`,
       }, // Setează token-ul Bearer în header
     });
-
     const bodyCart = await cartAPI.json();
-
     const addProduct2CartAPI = await request.post(
-      `${APIUrl}/carts/${bodyCart.id}`,
+      `${urlAPI}/carts/${bodyCart.id}`,
       {
         headers: {
           Authorization: `Bearer ${tokenLogin}`,
@@ -214,18 +216,15 @@ test.describe("API test", () => {
         data: { product_id: body.data[0].id, quantity: 1 }, // Setează token-ul Bearer în header
       }
     );
-
     const bodyCartContent = await addProduct2CartAPI.json();
-
-    const cartDetailsAPI = await request.get(`${APIUrl}/carts/${bodyCart.id}`, {
+    const cartDetailsAPI = await request.get(`${urlAPI}/carts/${bodyCart.id}`, {
       headers: {
         Authorization: `Bearer ${tokenLogin}`,
       },
     });
 
     const bodyCartDetails = await cartDetailsAPI.json();
-
-    const registerInvoiceAPI = await request.post(`${APIUrl}/invoices`, {
+    const registerInvoiceAPI = await request.post(`${urlAPI}/invoices`, {
       headers: {
         Authorization: `Bearer ${tokenLogin}`,
       },
@@ -240,22 +239,105 @@ test.describe("API test", () => {
       }, // Setează token-ul Bearer în header
     });
 
-    const bodyInvoiceContent = await registerInvoiceAPI.json();
+    expect(registerInvoiceAPI.status()).toBe(201);
 
+    const bodyInvoiceContent = await registerInvoiceAPI.json();
     console.log(bodyInvoiceContent.invoice_number);
 
-    const invoicesGet = await request.get(APIurl + "/invoices?page=1", {
+    const invoicesGet = await request.get(urlAPI + "/invoices?page=1", {
+      headers: {
+        Authorization: `Bearer ${tokenLogin}`, // Setează token-ul Bearer în header
+      },
+    });
+    expect(invoicesGet.status()).toBe(200);
+
+    const bodyInvoiceGet = await registerInvoiceAPI.json();
+    console.log(bodyInvoiceGet);
+
+    console.log(bodyCart);
+  });
+
+  test("view invoices", async ({ request }) => {
+    const response = await request.get(urlAPI + "/invoices?page=1", {
       headers: {
         Authorization: `Bearer ${tokenLogin}`, // Setează token-ul Bearer în header
       },
     });
 
-    expect(invoicesGet.status()).toBe(200);
+    expect(response.status()).toBe(200);
 
-    const bodyInvoiceGet = await registerInvoiceAPI.json();
+    const body = await response.json();
+    console.log(body.data);
 
-    console.log(bodyInvoiceGet);
+    // const invoicesNo = body.data.map((obiect: { user_id; id }) => ({
+    //   coloana222: obiect.user_id,
+    //   id22: obiect.id,
+    // }));
 
-    console.log(bodyCart);
+    const invoicesId2 = body.data.map(
+      (obiect: { invoice_number: string }) => obiect.invoice_number
+      // id22: obiect.id,
+    );
+    console.log(invoicesId2);
+  });
+
+  test("favorite", async ({ request }) => {
+    const response = await request.get(urlAPI + "/favorites", {
+      headers: {
+        Authorization: `Bearer ${tokenLogin}`,
+      },
+    });
+    expect(response.status()).toBe(200);
+
+    const body = await response.json();
+
+    const favorites = body.map((obiect) => obiect.product.price);
+    console.log(favorites);
+  });
+
+  test.only("me page", async ({ request }) => {
+    const response = await request.get(`${urlAPI}/users/me`, {
+      headers: {
+        Authorization: `Bearer${tokenLogin}`,
+      },
+    });
+
+    expect(response.status()).toBe(200);
+    // console.log(response.headers());
+
+    const body = await response.json();
+
+    console.log(body);
+
+    console.log(body.id);
+
+    // expect(body.phone).toBe("1111");
+
+    // const arrayUser = body.map((obiect) => obiect.dob);
+
+    // console.log(arrayUser);
+
+    const responsePUT = await request.put(`${urlAPI}/users/${body.id}`, {
+      headers: {
+        Authorization: `Bearer${tokenLogin}`,
+      },
+      data: {
+        first_name: "Jane",
+        last_name: "Doe",
+        email: "customer@practicesoftwaretesting.com",
+        phone: "11112222",
+        address: "Test street 98",
+        state: "XXX",
+        country: "Austria",
+        postcode: "111111",
+        city: "Vienna",
+      },
+    });
+
+    expect(responsePUT.status()).toBe(200);
+
+    const reponsePUTbody = await responsePUT.json();
+
+    console.log(reponsePUTbody);
   });
 });
